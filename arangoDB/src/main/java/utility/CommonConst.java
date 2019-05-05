ad603049828954f7fe23779bfa9a125e747fd135
dangnam8298 <44374063+dangnam8298@users.noreply.github.com>
2019-05-05 10:39:48 +0700

    Create CommonConst.java

---
+++ arangoDB/src/main/java/utility/CommonConst.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class CommonConst {
+	public class KEY_DB {
+		public static final String USER = "root";
+		
+		public static final String PASSWORD = "08021998";
+		
+		public static final String DATABASE = "arango_db";
+	}
+}
