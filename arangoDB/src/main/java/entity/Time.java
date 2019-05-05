0a7d3e477e24d5a143dd0a18631006849426a6c0
dangnam8298 <44374063+dangnam8298@users.noreply.github.com>
2019-05-05 10:39:41 +0700

    Create Time.java

---
+++ arangoDB/src/main/java/entity/Time.java
@@ -0,0 +1,17 @@
+package entity;
+
+import java.util.ArrayList;
+
+public class Time extends Entity {
+	public Time() {
+
+	}
+
+	public Time(String id, String label, String describe, ArrayList<Origin> origins) {
+		super(id, label, describe, origins);
+	}
+
+	public Time(Entity entity) {
+		super(entity.getId(), entity.getLabel(), entity.getDescribe(), entity.getOrigins());
+	}
+}
